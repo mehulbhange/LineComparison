1464e837049a7d84d65c75bd4d5cdc966c24f07e
mehulbhange <bhangemehul@gmail.com>
2022-08-10 15:29:46 +0530

    REFACTORED:[Mehul] line comparison using oop

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -6,28 +6,29 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
 
-        Double line1 = lengthOfLine();
+        LineComparison lineComparison = new LineComparison();
+
+        Double line1 = lineComparison.lengthOfLine();
 
         System.out.println("Length of line : " + line1);
 
-        Double line2 = lengthOfLine();
+        Double line2 = lineComparison.lengthOfLine();
+
+        //check if two lines are equal or not
+        if(lineComparison.isLineEquals(line1, line2))
+            System.out.println("Lines are equal");
+        else
+            System.out.println("Lines are not equal");
 
         //checking line1 is equal, greater or less than the other line.
-        int len = line1.compareTo(line2);
-        if( len < 0){
-            System.out.println("line1 is less than line2");
-        }else if( len > 0){
-            System.out.println( "line1 is greater than line2");
-        }else{
-            System.out.println("lines are equal");
-        }
+        lineComparison.compareLines(line1, line2);
 
     }
 
     /*
     * UC-1 -> calculate the length of line
     * */
-    public static double lengthOfLine(){
+    public double lengthOfLine(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter values for x1 and y1");
         double x1 = sc.nextDouble();
@@ -39,5 +40,21 @@ public class LineComparison {
 
         double lengthOfLine = Math.sqrt( Math.pow(x2-x1,2) + Math.pow(y2-y1,2) );
         return lengthOfLine;
+    }
+
+    public boolean isLineEquals(Double line1, Double line2){
+        return line1.equals(line2);
+    }
+
+    //compare lines
+    public void compareLines(Double line1, Double line2){
+        int len = line1.compareTo(line2);
+        if( len < 0){
+            System.out.println("line1 is less than line2");
+        }else if( len > 0){
+            System.out.println( "line1 is greater than line2");
+        }else{
+            System.out.println("lines are equal");
+        }
     }
 }
